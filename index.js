18cee7b0a99987bda1aacb8602c8458a41c38fbe
vjbravot <vicente.bravo@driv.in>
2024-05-26 11:56:41 -0400

    added dist

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+
 const app = express()
 app.use(express.json())
+app.use(cors())
 morgan.token('body', req => {
     return JSON.stringify(req.body)
   })
@@ -97,7 +100,7 @@ app.post('/api/persons', (request, response) => {
     response.status(204).end()
   })
   
-  const PORT = 3001
+  const PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
